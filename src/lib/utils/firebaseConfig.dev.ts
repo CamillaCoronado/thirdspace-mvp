3a95503e009dff845b8b179eb1be0bc96b59c2d1
Camilla Coronado <camillacoronado@Kiyomis-MacBook-Air.local>
2024-07-02 21:06:55 -0600

    loading with email broken

---
+++ src/lib/utils/firebaseConfig.dev.ts
@@ -0,0 +1,11 @@
+const firebaseDevConfig = {
+  apiKey: "AIzaSyDzwuDZjeIHb93qOHJ1AuWC1S0nDw7Ik58",
+  authDomain: "thirdplace-development-97a9a.firebaseapp.com",
+  projectId: "thirdplace-development-97a9a",
+  storageBucket: "thirdplace-development-97a9a.appspot.com",
+  messagingSenderId: "736373704408",
+  appId: "1:736373704408:web:2702ea282bf40fa5a3f9ad",
+  measurementId: "G-QNGDHFKK39"
+};
+
+export default firebaseDevConfig;
---
+++ src/lib/utils/firebaseConfig.prod.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC5ByxZK_nu0CgJKyvL_p8WWGnjeXt_Hdc",
+  authDomain: "thirdplace-c8aa9.firebaseapp.com",
+  projectId: "thirdplace-c8aa9",
+  storageBucket: "thirdplace-c8aa9.appspot.com",
+  messagingSenderId: "600544106823",
+  appId: "1:600544106823:web:9cc39b35c28b86e8c89462",
+  measurementId: "G-DW5JLQH09G"
+};
+
+export default firebaseConfig;
