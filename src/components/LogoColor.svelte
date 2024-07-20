c3f6122b14a32176bb3e5ad224adcb9558ab7052
Camilla Coronado <camilla.coronado@gmail.com>
2024-07-20 17:50:08 -0600

    switch out logo assets

--- src/components/LogoColor.svelte
+++ src/components/LogoColor.svelte
@@ -1,3 +1,3 @@
 <div class="my-64 block text-center">
-  <img class= "inline-block" style="width: 174px; height: 36px;" src="../src/assets/logo-color.png" alt="affinities" />
+  <img class= "inline-block" style="width: 174px; height: 36px;" src="../src/assets/logo-color.png" alt="thirdplace" />
 </div>
